c90c9d8b8acc22cef9822ac7a63bdbcb393883f8
강연규 <dmsghs18@naver.com>
2024-03-26 08:41:40 +0900

    1일차 프로젝트

---
+++ src/main/java/miniProj1/Users.java
@@ -0,0 +1,18 @@
+package miniProj1;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+	private String userid;
+	private String userpassword;
+	private String username;
+	private int    userage;
+	private String useremail;
+}
+
+
